a2e9cac596d2c9422f1ed72ad63fd5c9a30b5bcf
Student <Student>
2016-02-16 13:32:37 +0300

    first commit

---
+++ Myfirstapp/app/src/main/java/com/ruthmueni/myfirstapp/MathClass.java
@@ -0,0 +1,33 @@
+package com.ruthmueni.myfirstapp;
+
+/**
+ * Created by Student on 2/14/2016.
+ */
+public class MathClass {
+    public static void main(String arg[]) {
+       //calling variable by its name
+        addition();
+        subtraction();
+
+    }
+    //declare variables
+    public  static void addition(){
+
+        int b=7;
+        int c=5;
+
+        int answer =b+c;
+        System.out.println(answer);
+
+
+    }
+    //declare variables
+    public static void subtraction(){
+        int y=8;
+        int f=4;
+        int answer=y-f;
+        System.out.println(answer);
+
+    }
+
+}
